f00270d2045845324b40b9cce2777738af323f21
RiperPro03 <as.riperpro.pf@gmail.com>
2022-12-11 21:29:35 +0100

    correction accueil + rename en TEST

--- accueil-TEST.php
+++ accueil-TEST.php
@@ -1,5 +1,5 @@
 <?php
-    require 'includes/CarteJoueur.php';
+    require 'includes/Carte-Joueur.php';
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +9,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/accueil-TEST.css">
 </head>
 <body>
-    <link rel="stylesheet" href="css/accueil.css">
     <header>
         <nav>
             <ul>
