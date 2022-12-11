d62b789607cc7e7ee80fcf2f5e621e7121a7695f
HenriJez <henri.jezequel@etu.iut-tlse3.fr>
2022-12-11 15:16:17 +0100

    page d'accueil et debut php carte de joueurs

---
+++ CarteJoueur.php
@@ -0,0 +1,51 @@
+<?php
+    class CarteJoueur{
+        private $nom;
+        private $prenom;
+        private $pseudo;
+        private $poste;
+        private $image;
+
+        function __construct($nom, $prenom,$pseudo,$poste,$image) {
+            $this->nom = $nom;
+            $this->prenom = $prenom;
+            $this->pseudo = $pseudo;
+            $this->poste = $poste;
+            $this->image = $image;
+        }
+
+        function get_nom() {
+            return $this->nom;
+        }
+        
+        function get_prenom() {
+            return $this->prenom;
+        }
+        
+        function get_pseudo() {
+            return $this->pseudo;
+        }
+        
+        function get_poste() {
+            return $this->poste;
+        }
+
+        function get_carteJoueur(){
+            ?>
+            <div class="carte">
+                <div class="infoJoueur">
+                    <p>Pseudo : <?= $this->get_pseudo();?></p>
+                    <p>Nom : <?= $this->get_nom();?></p>
+                    <p>Prenom : <?= $this->get_prenom();?></p>
+                    <p>Poste : <?= $this->get_poste();?></p>
+                </div>
+                <div class="image">
+                    
+                </div>
+            </div>
+            
+            <?php
+        }
+
+    }
+?>
