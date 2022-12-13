6f34bcb78c1bf0866f9ddaf2e52c2abfc41a00e8
RiperPro03 <as.riperpro.pf@gmail.com>
2022-12-13 09:50:40 +0100

    Réorganisation des fichier

--- TEST-accueil.php
+++ TEST-accueil.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/accueil-TEST.css">
+    <link rel="stylesheet" href="css/TEST-accueil.css">
 </head>
 <body>
     <header>
--- test-nav-bar.php
+++ test-nav-bar.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/nav-bar-test.css">
+    <link rel="stylesheet" href="css/test-nav-bar.css">
 </head>
 <body>
     <nav>
