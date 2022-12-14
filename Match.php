d2ea4cee8595f3a227dd9d055ee3c2ba03c20f39
RiperPro03 <as.riperpro.pf@gmail.com>
2022-12-14 13:51:03 +0100

    ajout des page Equipe +Joueur + Mattch + Statistique

---
+++ Match.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="icon" href="vue-img.php?img=logo.png">
+    <link rel="stylesheet" href="./css/nav-bar.css">
+</head>
+<body>
+    <?php
+    require 'includes/nav-bar.html';
+    ?>
+</body>
+</html>
