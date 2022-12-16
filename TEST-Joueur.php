c571bc207d57086f64b8b60c44afe3e1ae384a46
RiperPro03 <as.riperpro.pf@gmail.com>
2022-12-16 21:50:10 +0100

    php barre de recherche

--- TEST-Joueur.php
+++ TEST-Joueur.php
@@ -1,9 +1,9 @@
 <?php
-    require './includes/db.php';
-    require 'includes/Carte-Joueur.php';
-    
-    global $db;
-    ?>
+require './includes/db.php';
+require 'includes/Carte-Joueur.php';
+
+global $db;
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -22,21 +22,41 @@
     <?php
     require 'includes/nav-bar.html';
     ?>
-    <div class="corps"> 
+    <div class="corps">
         <h1>Joueurs</h1>
+
         <div class="zoneRecherche">
-            <div class="BarreRecherche">
-                <input type="text" placeholder="Rechercher...">
-                <div class="recherche"></div>
-            </div>
+            <form action="<?php print $_SERVER['PHP_SELF']; ?>" method="get">
+                <div class="BarreRecherche">
+                    <input type="search" name="search" placeholder="Rechercher un Joueur">
+                    <div class="recherche"></div>
+                </div>
+            </form>
         </div>
+
         <div class="listeJoueurs">
-        <?php 
-                $q = $db->prepare('SELECT nom, prenom, pseudo, poste, photo FROM joueurs');
-                $q->execute();
+            <?php
+                if (isset($_GET['search'])) {
+                    if (empty($_GET['search'])) {
+                        $q = $db->prepare('SELECT nom, prenom, pseudo, poste, photo FROM joueurs');
+                        $q->execute();
+                    } else {
+                        $recherche = htmlspecialchars($_GET['search']);
+                        $q = $db->prepare('SELECT nom, prenom, pseudo, poste, photo FROM joueurs 
+                                            WHERE nom LIKE "%' . $recherche . '%" 
+                                            OR prenom LIKE "%' . $recherche . '%" 
+                                            OR pseudo LIKE "%' . $recherche . '%" 
+                                            OR poste LIKE "%' . $recherche . '%"');
+                        $q->execute();
+                    }
+                } else {
+                    $q = $db->prepare('SELECT nom, prenom, pseudo, poste, photo FROM joueurs');
+                    $q->execute();
+                }
+
                 if ($q->rowCount() > 0) {
                     while ($joueur = $q->fetch()) {
-                        $joueur = new CarteJoueur($joueur['nom'], $joueur['prenom'], $joueur['pseudo'], $joueur['poste'], $joueur['photo'],0,0);
+                        $joueur = new CarteJoueur($joueur['nom'], $joueur['prenom'], $joueur['pseudo'], $joueur['poste'], $joueur['photo'], 0, 0);
                         echo $joueur->get_carteJoueurAccueil();
                     }
                 } else {
