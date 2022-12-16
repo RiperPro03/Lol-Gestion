98727e342645e46211a19f4e24c0e6c6e7373728
HenriJez <henri.jezequel@etu.iut-tlse3.fr>
2022-12-16 09:28:40 +0100

    modification cartejoueurs

--- TEST-CarteJoueur.php
+++ TEST-CarteJoueur.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TEST-CarteEquipe</title>
+    <script src="https://kit.fontawesome.com/acf8d5192c.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./css/nav-bar.css">
     <link rel="stylesheet" href="./css/TEST-carteJoueur.css">
     <link rel="stylesheet" href="./css/carteJoueur.css">
@@ -21,11 +22,37 @@
             <a href="./TEST-CarteEquipe.php"> test carte Equipe</a>
             <a href="./TEST-CarteMatch.php">test carte Match</a>
         </div>
+        
         <?php
             $image = '637a7214430be5.13956557.png';
             $joueur = new CarteJoueur('su','lee','Lebon','Bot',$image,'12','15');
             echo $joueur->get_carteJoueurAccueil(); 
         ?>
+       
+       <div class="carteJoueur">
+            <div class="contourJoueur">
+            </div>
+            <div class="boiteimageJoueur">
+                <div class="imageJoueur">
+                    <img src="vue-img.php?img=637a7214430be5.13956557.png"style="width:100%;"> 
+                </div>
+            </div>
+            <div class="boiteInfoJoueur">
+                <div class="detailsJoueur">
+                    <h2> faker <br><span>lee see Hyuang </span></h2>
+                    <div class="infoJoueur">
+                        <h3> Poste<br> <span>mid</span></h3>
+                        <h3> Victoire<br><span>11</span></h3>                        
+                        <h3> Selection<br><span>15</span>
+                    </div>
+                    <div class="option">
+                        <a href="#" class="bouton"><i class="fa-solid fa-pen"></i></a>
+                        <a href="#2" class="bouton"><i class="fa-solid fa-trash"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </body>
 </html>
