7cd31eed7815b9b7213d43a7b42bafd3eded2c7e
HenriJez <henri.jezequel@etu.iut-tlse3.fr>
2022-12-16 10:19:34 +0100

    modification des carte

--- TEST-CarteMatch.php
+++ TEST-CarteMatch.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/acf8d5192c.js" crossorigin="anonymous"></script>
     <title>TEST-CarteMatch</title>
     <link rel="stylesheet" href="./css/nav-bar.css">
     <link rel="stylesheet" href="./css/TEST-carteMatch.css">
